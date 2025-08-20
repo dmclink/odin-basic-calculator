a0b8a9af1da76009c2a68f662bae4a3ca989e15f
dmclink <135415819+dmclink@users.noreply.github.com>
2025-08-20 19:34:11 +0800

    feat: add boilerplate for buttonPress event listener

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -44,11 +44,51 @@ function divide(a, b) {
 class Calculator {
 	constructor() {
 		this.display = document.querySelector('#display');
-		this.param1 = null;
-		this.param2 = null;
+		this.displayString = '';
+		this.params = [];
 		this.operator = null;
 		this.input = '';
+		this.dotPressed = false;
+	}
+
+	/** Calls the appropriate function or updates params depending on the button pressed.
+	 * Number buttons will be concatenated with the top param.
+	 * Operators will either start a new param or result in calculation if an operator already exists.
+	 * Equal sign will result in calculation.
+	 * mc will clear all params and operators.
+	 *
+	 * @param {string} val - the value of the button that was pressed
+	 */
+	buttonPress(val) {
+		switch (val) {
+			case ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'):
+				//TODO:
+				break;
+
+			case ('+', '-', '*', '/'):
+				//TODO:
+				break;
+
+			case '=':
+				//TODO:
+				break;
+
+			case 'mc':
+				//TODO:
+				break;
+
+			case '.':
+				//TODO:
+				break;
+		}
 	}
 }
 
 const calc = new Calculator();
+
+const calcElement = document.querySelector('#calculator');
+calcElement.addEventListener('click', (e) => {
+	if (e.target.classList.contains('btn')) {
+		calc.buttonPress(e.target.textContent);
+	}
+});
